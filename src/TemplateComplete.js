4c90e30e1184dd73163908a8becac39007a9f0f8
Ahmad Iqbal <ahmadiqbal@tobaconsulting.com>
2021-01-20 17:36:47 +0700

    initial commit

---
+++ src/TemplateComplete.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import copy from "copy-to-clipboard";
+
+const RenderTemplate = (selection, words) => {
+    switch (selection) {
+        case 'keqing':
+            return `eda cuma pengen jilat ${words}, soalnya ${words} adalah kesukaan eda`
+        case 'venti':
+            return `eda pergi buat nampol ${words}, darikemaren didiemin si ${words} batu bener`
+        case 'qiqi':
+            return `eda ke pasar nyariin ${words}, kemaren belum bayar utang si ${words}`
+        case 'ganyu':
+            return `eda ditampolin sama ${words}, trus eda lari dari kejaran ${words}`
+        default:
+            return "";
+    }
+}
+
+function TemplateComplete(props) {
+
+    const FinishedWords = RenderTemplate(props.selection, props.words)
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div>{FinishedWords}</div>
+            </div>
+            <div className="row mt-5">
+                <Button variant="primary" onClick={() => copy(FinishedWords)}>Copy To Clipboard</Button>{' '}
+            </div>
+        </div>
+    );
+}
+
+export default TemplateComplete;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TemplateComplete from './TemplateComplete';
+
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [words, setWords] = useState('');
+  const [selection, setSelection] = useState('keqing')
+  
+  // eslint-disable-next-line
+  const [template, setTemplate] = useState('');
+
+
+  const onChange = (event) => {
+    setWords(event.target.value);
+
+  };
+  useEffect(() => { setSelection('keqing') }, []);
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <label>
+            Name:
+          </label>
+          <input class="form-control" type="text" value={words} onChange={onChange} />
+        </div>
+        <div className="col">
+          <label>
+            Template
+          </label>
+          <select class="form-control" onChange={e => setSelection(e.target.value)}>
+              <option selected value="keqing">template 1</option>
+              <option value="venti">template 2</option>
+              <option value="ganyu">template 3</option>
+              <option value="qiqi">template 4</option>
+            </select>
+
+        </div>
+
+        {console.log(selection)}
+        {/* submit template */}
+
+      </div>
+
+      <div className="row">
+        {template}
+        <TemplateComplete selection={selection} words={words} />
+      </div>
+      <br />
+
+    </div>
+  );
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
