08ec7f5b34a1bfdbca57a09b982b55b2513b96c8
dogepism <blazinteostra@gmail.com>
2021-01-21 16:09:26 +0700

    added more template

--- src/TemplateComplete.js
+++ src/TemplateComplete.js
@@ -6,14 +6,14 @@ import copy from "copy-to-clipboard";
 
 const RenderTemplate = (selection, words) => {
     switch (selection) {
-        case 'keqing':
+        case 'option1':
             return `eda cuma pengen jilat ${words}, soalnya ${words} adalah kesukaan eda`
-        case 'venti':
+        case 'option2':
             return `eda pergi buat nampol ${words}, darikemaren didiemin si ${words} batu bener`
-        case 'qiqi':
-            return `eda ke pasar nyariin ${words}, kemaren belum bayar utang si ${words}`
-        case 'ganyu':
-            return `eda ditampolin sama ${words}, trus eda lari dari kejaran ${words}`
+        case 'option3':
+            return `${words}........... ${words} ${words} AAAAAAAAAAAAAAAAAGH AAAAAAAAAAAAAAAAAAAAAAAGH WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya ${words} wangi aku mau nyiumin aroma wanginya ${words} AAAAAAAAH - Rambutnya.... aaah rambutnya juga pengen aku elus-elus - AAAAAH ${words} keluar pertama kali juga manis dia bawa balon           itu juga manis banget AAAAAAAAH ${words} LUCCUUUUUUUUUUUUUUU...... GUA BAKAL BAKAR DUIT 12 JUTA RUPIAH BUAT ${words} AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH apa ? ${words} itu gak nyata ? Cuma karakter 2 dimensi katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK CYGAMES BANGSAAAAAT!! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ${words} ngeliat gw ... ${words} di laptop ngeliatin gw ${words} ... kamu percaya sama aku ? aaaaaaaaaaah syukur ${words} gak malu merelakan aku aaaaaah YEAAAAAAAAAAAH GUA MASIH PUNYA ${words}, SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH ANILA, ANCHIRA MAHIRA VAJRA KUMBIRAAAAA KIRIMKANLAH CINTAKU PADA ${words} KIRIMKAN KE CYGAMES AAAAAAAAH`
+        case 'option4':
+            return `NENEN NENEN KEPENGEN NENEN SAMA ${words}. TETEK GEDE NAN KENCANG MILIK ${words} MEMBUATKU KEPENGEN NENEN. DIBALUT PAKAIAN KETAT YANG ADUHAI CROOOOTOTOTOTOTOT ANJING SANGE GUA BANGSAT. ${words},PLIS DENGERIN BAIK BAIK. TOLONG BUKA BAJU SEBENTAR SAJA PLISSS TOLOOONG BANGET,BIARKAN MULUT KERINGKU BISA MENGECAP NENEN ${words}. BIARKAN AKU MENGENYOT NENENMU ${words}. AKU RELA NGASIH SESEMBAHAN APA AJA BERAPAPUN ITU DUIT YG AKU BAKAR KHUSUS TERKHUSUS BUATMU. TAPI TOLOOOONG BANGET BUKA BAJUMU AKU MAU NENEN.NENEN NENEEEEN NENEN ${words} WANGIIII`
         default:
             return "";
     }
--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ import { GlobalStyles } from './global';
 function App() {
   // Declare a new state variable, which we'll call "count"
   const [words, setWords] = useState('');
-  const [selection, setSelection] = useState('keqing')
+  const [selection, setSelection] = useState('option1')
 
   // eslint-disable-next-line
   const [template, setTemplate] = useState('');
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
   const themeToggler = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light')
   }
@@ -26,7 +26,7 @@ function App() {
     setWords(event.target.value);
 
   };
-  useEffect(() => { setSelection('keqing') }, []);
+  useEffect(() => { setSelection('option1'); setWords('keqing') }, []);
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
@@ -34,15 +34,15 @@ function App() {
         <GlobalStyles />
         <div>
           <nav className={"navbar navbar-expand-lg" + (theme === 'light' ? " navbar-light bg-light" : " navbar-dark bg-dark")}>
-            <a className="navbar-brand" href="#">Wangy Generator</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a className="navbar-brand" href="#">Degen-erator</a>
+            {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
+            </button> */}
+            <div className="ml-auto" id="navbarNav">
+              <ul className="navbar-nav ">
                 <li className="nav-item active">
                   <button
-                    className={"btn "+(theme === 'light' ? "btn-light":"btn-dark")}
+                    className={"btn " + (theme === 'light' ? "btn-light" : "btn-dark")}
                     onClick={themeToggler}
                   >
                     Switch Theme
@@ -67,10 +67,10 @@ function App() {
                   Template
                 </label>
                 <select defaultValue="keqing" className="form-control" onChange={e => setSelection(e.target.value)}>
-                  <option value="keqing">template 1</option>
-                  <option value="venti">template 2</option>
-                  <option value="ganyu">template 3</option>
-                  <option value="qiqi">template 4</option>
+                  <option value="option1">template 1</option>
+                  <option value="option2">template 2</option>
+                  <option value="option3">template 3</option>
+                  <option value="option4">template 4</option>
                 </select>
               </div>
             </div>
@@ -85,6 +85,15 @@ function App() {
             <div className="row">
             </div>
           </div>
+        </div>
+        <div className="img-responsive justify-content-md-center text-center my-5">
+          <a target="_blank" href="https://static.wikia.nocookie.net/meme/images/5/51/FC6B143C-9DF6-4D2D-91B1-CC2E4A1368A6.jpeg/revision/latest/scale-to-width-down/220?cb=20200818155515" className="img-responsive">
+            <img className="img-judi" src={"https://cdn.indxxi.xyz/wp-content/uploads/2020/06/25132817/600x350-lux138-Pop-Up.gif"} alt="Logo" />
+          </a>
+        </div>
+
+        <div class={"text-center footer p-3 mt-5 " + (theme === 'light' ? "text-dark" : "text-white")}>
+          © {(new Date().getFullYear())} Copyright: DOGEPISM. All Rights Reserved 
         </div>
       </>
     </ThemeProvider>
